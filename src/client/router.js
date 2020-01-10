f0b5e324f66f7ef3a86735a6e5f773d490c89267
Katon Surya Atmaja <katonatmaja@gmail.com>
2020-01-10 09:48:10 +0700

    Update route guard

--- src/client/router.js
+++ src/client/router.js
@@ -16,10 +16,15 @@ import store from './store'
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '/profile', component: Profile },
+  {
+    path: '/profile',
+    component: Profile,
+    meta: { requiredAuthentication: true}
+  },
   {
     path: '/exam/:id',
     component: Exam,
+    meta: { requiredAuthentication: true },
     children: [
       {
         path: 'review',
@@ -37,14 +42,24 @@ const routes = [
   {
     path: '/',
     component: Home,
-    beforeEnter: async function (to, from, next) {
-      fetch('/api/auth/information', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ authenticated: store.state.authenticated}),
-      })
+    meta: { requiredAuthentication: true },
+    
+  },
+];
+
+const router = new VueRouter({ routes });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiredAuthentication)) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    fetch('/api/auth/information', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ authenticated: store.state.authenticated }),
+    })
       .then(res => res.json())
       .then(res => {
         if (res.error)
@@ -52,9 +67,9 @@ const routes = [
         else
           next();
       })
-    }
-  },
-];
+  } else {
+    next() // make sure to always call next()!
+  }
+})
 
-const router = new VueRouter({ routes });
 export default router;
