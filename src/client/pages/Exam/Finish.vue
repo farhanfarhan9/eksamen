4e7d4bdc9f8b5ce39b3b853e9d03539cf8b12ec2
Tinesh14 <47784927+Tinesh14@users.noreply.github.com>
2020-01-10 10:04:08 +0700

    tambah soal ujian

--- src/client/pages/Exam/Finish.vue
+++ src/client/pages/Exam/Finish.vue
@@ -1,19 +1,42 @@
 <template>
   <!-- Card -->
-  <div class="bg-white rounded shadow">
+  <div class="flex justify-center">
+  <div class="md:w-8/12 lg:w-6/12 bg-white rounded shadow">
 
     <!-- Card Body -->
     <div class="px-4 py-2">
       <h2 class="text-2xl">
-        Finish Ujian {{ this.$route.params.id }}
+        Finish Ujian Bahasa Inggris
       </h2>
-
+       <div class="mx-8">
+          <table>
+            <tbody >
+              <tr>
+                <th class="py-5 text-left px-20">Jumlah Soal : </th>
+                <td class="py-5">X</td>
+              </tr>
+              <tr>
+                <th class="text-left px-20">Jumlah Soal Yang Dikerjakan : </th>
+                <td class="text-right">X</td>
+              </tr>
+              <tr>
+                <th class="py-5 text-left px-20">Sisa Waktu : </th>
+                <td class="py-5 text-right">X</td>
+              </tr>
+              <tr>
+                <th class="text-left px-20">Total Score : </th>
+                <td>X</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       <div>
-        <button @click="pergiKeBeranda">
+        <button @click="pergiKeBeranda" class="w-full p-2 bg-pink-400 text-white border border-pink-400 rounded mt-8">
           Kembali ke Menu
         </button>
       </div>
     </div>
+  </div>
   </div>
 </template>
 
